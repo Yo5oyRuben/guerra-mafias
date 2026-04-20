58048d5a6f4311a3646a0013284070dca39e55b2
Yo5oyRuben <ruben@dibamaro.eu>
2026-04-20 12:56:30 +0200

    MMD update

--- c/config.h
+++ c/config.h
@@ -8,16 +8,19 @@
 #define T_MCS 100
 #define T_MAX 2000
 #define W 200
+#define NDIV 50
 
 /* Parametros ER por bloques */
 #define P11 (6.0/(N1-1.0))
-#define P12 0.0
+#define P12 0.02
 #define P22 (6.0/(N2-1.0))
 
 /*parametros de lambda*/
 #define R 0
 #define E -0.4
 #define B 2
+#define BMIN 1
+#define BMAX 3
 
 /*para debug*/
 #define SAVE_SERIES 0
--- c/run_well_mixed.c
+++ c/run_well_mixed.c
@@ -26,19 +26,30 @@ int main(void)
     u_min_max u;
     uint8_t gamma[NTOT];
 
+    int i;
     double xc_1_med,xc_2_med,xc_1_sigma,xc_2_sigma,c_med,c_sigma;
     unsigned int seed_graph=(unsigned int)time(NULL);
     unsigned int seed_state=(unsigned int)time(NULL)*2;
+    double c_med_values[NDIV];
+    double delta=(double)(BMAX-BMIN)/NDIV;
 
-    lambda.b=B; lambda.e=E; lambda.r=R;
+
+    lambda.b=BMIN; lambda.e=E; lambda.r=R;
     initial_ER(&g,P11,P12,P22,seed_graph);
     rng_seed(seed_state);
     ini_rand_Pr(gamma,0.5,0.5);
     u_bounds(lambda,&u);
 
-    time_loop(gamma,&g,lambda,u,&xc_1_med,&xc_2_med,&xc_1_sigma,&xc_2_sigma,&c_med,&c_sigma);
-    printf("<c>=%f",c_med);
+    for(i=0;i<NDIV;i++)
+    {
+        ini_rand_Pr(gamma,0.5,0.5);
+        time_loop(gamma, &g, lambda, u, &xc_1_med, &xc_2_med, &xc_1_sigma, &xc_2_sigma, &c_med, &c_sigma);
+        c_med_values[i]=c_med;
+        printf("%f\t%f\n",lambda.b,c_med);
+        lambda.b+=delta;
+        u_bounds(lambda, &u);
+    }
 
-    free(g.col_idx);
+        free(g.col_idx);
     return 0;
 }
